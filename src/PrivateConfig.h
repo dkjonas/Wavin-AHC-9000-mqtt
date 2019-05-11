2c8f1e6262a5312a29fa3c2c7e3dec780593954b
dkjonas <26826706+dkjonas@users.noreply.github.com>
2019-05-11 22:15:52 +0200

    Added comment about empty username/password
    Fix #10

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -4,5 +4,5 @@ const String   WIFI_SSID = "Enter wireless SSID here";         // wifi ssid
 const String   WIFI_PASS = "Enter wireless password here";     // wifi password
 
 const String   MQTT_SERVER = "Enter mqtt server address here"; // mqtt server address without port number
-const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user
-const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password
+const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user. Use "" for no username
+const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password. Use "" for no password
