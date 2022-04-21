91b38011c781c1368259df4476286937e0b3c7e9
Heine Pedersen <nox@nox.dk>
2022-04-21 22:13:07 +0200

    Added support for default min/max/step temperature

--- src/PrivateConfig.h
+++ src/PrivateConfig.h
@@ -7,3 +7,7 @@ const String   MQTT_SERVER = "Enter mqtt server address here"; // mqtt server ad
 const String   MQTT_USER   = "Enter mqtt username here";       // mqtt user. Use "" for no username
 const String   MQTT_PASS   = "Enter mqtt password here";       // mqtt password. Use "" for no password
 const uint16_t MQTT_PORT   = 1883;                             // mqtt port
+
+const float    MIN_TEMP    = 6.0;                              // minimum temperature to set
+const float    MAX_TEMP    = 40.0;                             // maximum temperature to set
+const float    TEMP_STEP   = 0.5;                              // the temperature step size, either 0.5 or 1.0
